8a37cb1dae6031063914155f2db05c31bb87bf66
Vladimir Reshetnikov <zepplock@yahoo.com>
2011-11-01 09:57:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubystand::Application.config.secret_token = 'b78b93d164b36f7f3286acabb9d7fb0baeee9f4f7abbfdc9fd0a19589eaa534cb256f587726b6ea9fda146a76109095de4d5e305479e453b8939f9684617fb35'
