0f321381a1f2bdcf5259e929c2c946b6da2f8184
Vladimir Reshetnikov <zepplock@yahoo.com>
2011-11-02 17:19:25 -0700

    Attribution

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module Rubystand
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.rubystand = YAML.load(File.open('data/rubystand.yaml'))
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
@@ -46,7 +48,9 @@ module Rubystand
 
     config.assets.paths << "#{Rails.root}/data"
 
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
   end
 end
